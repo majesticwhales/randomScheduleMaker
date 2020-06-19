5ca6c3837ef0f1390a52d4db4de569c30ec011bb
majesticwhales <maxodell16@gmail.com>
2020-06-19 15:32:17 -0400

    setup issues

--- setup.c
+++ setup.c
@@ -9,11 +9,13 @@ int basicSetup(int *teams, int *games, int *divs) {
 
   printf("How many games would you like to have in the season?\n");
   if (scanf("%d", &games) != 1 || games < 1) {
-    return 2;
+    return_code = 2;
   }
 
   printf("How many divisions would you like to have in one league?\n");
   if (scanf("%d", &divs) != 1 || divs < 1) {    
-    return 3;
+    return_code = 3;
   }
+
+  return return_code;
 }

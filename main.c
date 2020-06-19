9864460efce081f8f71f61b97541dd7831706c78
majesticwhales <maxodell16@gmail.com>
2020-06-19 15:26:45 -0400

    more additions

--- main.c
+++ main.c
@@ -3,30 +3,15 @@
 
 int main(void) {
 
-    int divs = 0;
-    int teams = 0;
-    int games = 0;
+    int *divs;
+    int *teams;
+    int *games;
 
-    printf("How many teams/participants do you want to include?:\n");
+    basicSetup(divs, teams, games);
 
-    if (scanf("%d", &teams) != 1 || teams < 2) {
-        printf("Error: You must have at least two teams or participants.\n");
-        return 1;
-    }
-
-    printf("How many games would you like to have in the season?\n");
-
-    if (scanf("%d", &games) != 1 || games < 1) {
-        printf("Error: You must have at least on egame in the regular season for each team.\n");
-        return 2;
-    }
-
-    printf("How many divisions would you like to have in one league?\n");
-
-    if (scanf("%d", &divs) != 1 || divs < 1) {
-        printf("Error: You must have at least one division.\n");
-        return 3;
-    }
-
+    printf("Error: You must have at least two teams or participants.\n");
+    printf("Error: You must have at least on egame in the regular season for each team.\n");
+    printf("Error: You must have at least one division.\n");
+    
     return 0;
 }
---
+++ setup.c
@@ -0,0 +1,19 @@
+int basicSetup(int *teams, int *games, int *divs) {
+
+  int return_code = 0;
+
+  printf("How many teams would you like to have in your league?\n");
+  if (scanf("%d", &teams) != 1 || teams < 2) { 
+    return_code = 1;
+  }
+
+  printf("How many games would you like to have in the season?\n");
+  if (scanf("%d", &games) != 1 || games < 1) {
+    return 2;
+  }
+
+  printf("How many divisions would you like to have in one league?\n");
+  if (scanf("%d", &divs) != 1 || divs < 1) {    
+    return 3;
+  }
+}

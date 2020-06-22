229dc6c557a59d7d1d023c7dcee9a1ffa9e760cf
majesticwhales <maxodell16@gmail.com>
2020-06-22 10:43:09 -0400

    huge updates, got everything working (thanks seashell)

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
 #include "setup.c"
+#include <stdlib.h>
 
 int main(void) {
 
-    int *divs;
-    int *teams;
-    int *games;
+    int *divs = malloc(sizeof(int));
+    int *teams = malloc(sizeof(int));
+    int *games = malloc(sizeof(int));
 
     int error = basicSetup(divs, teams, games);
 
     if (error_analysis(error)) {
         return error;
     }
+  
+    free(divs);
+    free(teams);
+    free(games);
 
     return 0;
 }
--- setup.c
+++ setup.c
@@ -1,19 +1,21 @@
+#include <stdio.h>
+
 int basicSetup(int *teams, int *games, int *divs) {
 
   int return_code = 0;
 
   printf("How many teams would you like to have in your league?\n");
-  if (scanf("%d", &teams) != 1 || *teams < 2) { 
+  if (scanf("%d", teams) != 1 || *teams < 2) { 
     return_code = 1;
   }
 
   printf("How many games would you like to have in the season?\n");
-  if (scanf("%d", &games) != 1 || *games < 1) {
+  if (scanf("%d", games) != 1 || *games < 1) {
     return_code = 2;
   }
 
   printf("How many divisions would you like to have in the league?\n");
-  if (scanf("%d", &divs) != 1 || *divs < 1) {    
+  if (scanf("%d", divs) != 1 || *divs < 1) {    
     return_code = 3;
   }
 

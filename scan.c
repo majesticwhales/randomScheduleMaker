7d98bf6815a1ef36a55a4203be1be9f1696e0829
majesticwhales <maxodell16@gmail.com>
2020-06-23 16:06:25 -0400

    scan beginning

--- scan.c
+++ scan.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
 void scan_answer(char *c) {
-  
+  switch(*c) {
+    case 't':
+      //team
+      break;
+    case 'd':
+      //division
+      break;
+    case 's':
+      //schedule
+      break;
+    case 'f':
+      //makesched
+      break;
+    case 'q':
+      break;
+    default:
+      //re-do
+  }
 }

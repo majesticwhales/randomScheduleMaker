96f7a3c091c09e8acdcebc0b4f179dd49be4af8a
majesticwhales <maxodell16@gmail.com>
2020-07-02 15:28:46 -0400

    team

--- team.c
+++ team.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+struct team {
+  int id;
+  char *name;
+  int *schedule;
+}
+
+
+struct team team_create {
+
+}
+
 void team_functions(void) {
-  
+  printf("Welcome to team functions!");
+
 }
